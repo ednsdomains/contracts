ae5c7f2da610b8b84ec22d68afb3932d00346c92
Miklet <iam@mikeng.io>
2022-06-29 20:16:05 +0800

    Updated helper scripts

--- scripts/helpers/deploy.ts
+++ scripts/helpers/deploy.ts
@@ -1,7 +1,20 @@
 import { ethers, upgrades } from "hardhat";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import NetworkConfig, { Network } from "../../network.config";
-import { IContracts } from "../interfaces/contracts";
+import {
+  DomainPriceOracle,
+  OmniRegistrar,
+  OmniRegistrarController,
+  OmniRegistrarSynchronizer,
+  PublicResolver,
+  PublicResolverSynchronizer,
+  Registry,
+  Root,
+  SingletonRegistrar,
+  SingletonRegistrarController,
+  Token,
+  TokenPriceOracle,
+} from "../../typechain";
 
 export interface IDeployInput {
   signer: SignerWithAddress;
@@ -9,14 +22,17 @@ export interface IDeployInput {
   networks: Network[];
 }
 
-export async function deploy(input: IDeployInput): Promise<IContracts> {
-  // ERC-20 Token
+// ERC-20 Token
+export async function deployToken(input: IDeployInput): Promise<Token> {
   const TokenFactory = await ethers.getContractFactory("Token", input.signer);
   const _token = await upgrades.deployProxy(TokenFactory, [NetworkConfig[input.network].layerzero.chainId, NetworkConfig[input.network].layerzero.endpoint.address]);
   const token = TokenFactory.attach(_token.address);
   console.log(`[[${NetworkConfig[input.network].name}]] Token Deployed - ${token.address}`);
+  return token;
+}
 
-  // Token Price Oracle
+// Token Price Oracle
+export async function deployTokenPriceOracle(input: IDeployInput): Promise<TokenPriceOracle> {
   const TokenPriceOracleFactory = await ethers.getContractFactory("TokenPriceOracle", input.signer);
   const tokenPriceOracle = await TokenPriceOracleFactory.deploy(
     NetworkConfig[input.network].chainlink.token.address,
@@ -25,22 +41,35 @@ export async function deploy(input: IDeployInput): Promise<IContracts> {
   );
   await tokenPriceOracle.deployed();
   console.log(`[[${NetworkConfig[input.network].name}]] TokenPriceOracle Deployed - ${tokenPriceOracle.address}`);
+  return tokenPriceOracle;
+}
 
-  // Domain Price Oracle
+export interface IDeployTokenPriceOracleInput extends IDeployInput {
+  TokenPriceOracle: TokenPriceOracle;
+}
+
+// Domain Price Oracle
+export async function deployDomainPriceOracle(input: IDeployTokenPriceOracleInput): Promise<DomainPriceOracle> {
   const DomainPriceOracleFactory = await ethers.getContractFactory("DomainPriceOracle", input.signer);
-  const _domainPriceOracle = await upgrades.deployProxy(DomainPriceOracleFactory, [tokenPriceOracle.address]);
+  const _domainPriceOracle = await upgrades.deployProxy(DomainPriceOracleFactory, [input.TokenPriceOracle.address]);
   await _domainPriceOracle.deployed();
   const domainPriceOracle = DomainPriceOracleFactory.attach(_domainPriceOracle.address);
   console.log(`[[${NetworkConfig[input.network].name}]] DomainPriceOracle Deployed - ${domainPriceOracle.address}`);
+  return domainPriceOracle;
+}
 
-  // Registry
+// Registry
+export async function deployRegistry(input: IDeployInput): Promise<Registry> {
   const RegistryFactory = await ethers.getContractFactory("Registry", input.signer);
   const _registry = await upgrades.deployProxy(RegistryFactory);
   await _registry.deployed();
   const registry = RegistryFactory.attach(_registry.address);
   console.log(`[[${NetworkConfig[input.network].name}]] Registry Deployed - ${registry.address}`);
+  return registry;
+}
 
-  // Public Resolver Synchronizer
+// Public Resolver Synchronizer
+export async function deployPublicResolverSynchronizer(input: IDeployInput): Promise<PublicResolverSynchronizer> {
   const PublicResolverSynchronizerFactory = await ethers.getContractFactory("PublicResolverSynchronizer", input.signer);
   const _publicResolverSynchronizer = await upgrades.deployProxy(PublicResolverSynchronizerFactory, [
     NetworkConfig[input.network].layerzero.endpoint.address,
@@ -50,35 +79,63 @@ export async function deploy(input: IDeployInput): Promise<IContracts> {
   await _publicResolverSynchronizer.deployed();
   const publicResolverSynchronizer = PublicResolverSynchronizerFactory.attach(_publicResolverSynchronizer.address);
   console.log(`[[${NetworkConfig[input.network].name}]] PublicResolverSynchronizer Deployed - ${publicResolverSynchronizer.address}`);
+  return publicResolverSynchronizer;
+}
 
-  // Public Resolver
-  const PublicResolver = await ethers.getContractFactory("PublicResolver", input.signer);
-  const _publicResolver = await upgrades.deployProxy(PublicResolver, [registry.address, publicResolverSynchronizer.address], { unsafeAllow: ["delegatecall"] });
+export interface IDeployPublicResolverInput extends IDeployInput {
+  Registry: Registry;
+  PublicResolverSynchronizer: PublicResolverSynchronizer;
+}
+
+// Public Resolver
+export async function deployPublicResolver(input: IDeployPublicResolverInput): Promise<PublicResolver> {
+  const PublicResolverFactory = await ethers.getContractFactory("PublicResolver", input.signer);
+  const _publicResolver = await upgrades.deployProxy(PublicResolverFactory, [input.Registry.address, input.PublicResolverSynchronizer.address], { unsafeAllow: ["delegatecall"] });
   await _publicResolver.deployed();
-  const publicResolver = PublicResolver.attach(_publicResolver.address);
+  const publicResolver = PublicResolverFactory.attach(_publicResolver.address);
   console.log(`[[${NetworkConfig[input.network].name}]] PublicResolver Deployed - ${publicResolver.address}`);
+  return publicResolver;
+}
 
-  // Singleton Registrar
+export interface IDeploySingletonRegistrarInput extends IDeployInput {
+  Registry: Registry;
+}
+
+// Singleton Registrar
+export async function deploySingletonRegistrar(input: IDeploySingletonRegistrarInput): Promise<SingletonRegistrar> {
   const SingletonRegistrarFactory = await ethers.getContractFactory("SingletonRegistrar", input.signer);
-  const _singletonRegistrar = await upgrades.deployProxy(SingletonRegistrarFactory, [registry.address]);
+  const _singletonRegistrar = await upgrades.deployProxy(SingletonRegistrarFactory, [input.Registry.address]);
   await _singletonRegistrar.deployed();
   const singletonRegistrar = SingletonRegistrarFactory.attach(_singletonRegistrar.address);
   console.log(`[[${NetworkConfig[input.network].name}]] SingletonRegistrar Deployed - ${singletonRegistrar.address}`);
+  return singletonRegistrar;
+}
 
-  // Singleton Registrar Controller
+export interface IDeploySingletonRegistrarControllerInput extends IDeployInput {
+  Token: Token;
+  DomainPriceOracle: DomainPriceOracle;
+  TokenPriceOracle: TokenPriceOracle;
+  SingletonRegistrar: SingletonRegistrar;
+}
+
+// Singleton Registrar Controller
+export async function deploySingletonRegistrarController(input: IDeploySingletonRegistrarControllerInput): Promise<SingletonRegistrarController> {
   const SingletonRegistrarControllerFactory = await ethers.getContractFactory("SingletonRegistrarController", input.signer);
   const _singletonRegistrarController = await upgrades.deployProxy(SingletonRegistrarControllerFactory, [
-    token.address,
-    domainPriceOracle.address,
-    tokenPriceOracle.address,
-    singletonRegistrar.address,
+    input.Token.address,
+    input.DomainPriceOracle.address,
+    input.TokenPriceOracle.address,
+    input.SingletonRegistrar.address,
     NetworkConfig[input.network].slip44.coinId,
   ]);
   await _singletonRegistrarController.deployed();
   const singletonRegistrarController = SingletonRegistrarControllerFactory.attach(_singletonRegistrarController.address);
   console.log(`[[${NetworkConfig[input.network].name}]] SingletonRegistrarController Deployed - ${singletonRegistrarController.address}`);
+  return singletonRegistrarController;
+}
 
-  // Omni Registrar Synchronizer
+// Omni Registrar Synchronizer
+export async function deployOmniRegistrarSynchronizer(input: IDeployInput): Promise<OmniRegistrarSynchronizer> {
   const OmniRegistrarSynchronizerFactory = await ethers.getContractFactory("OmniRegistrarSynchronizer", input.signer);
   const _omniRegistrarSynchronizer = await upgrades.deployProxy(OmniRegistrarSynchronizerFactory, [
     NetworkConfig[input.network].layerzero.endpoint.address,
@@ -88,33 +145,60 @@ export async function deploy(input: IDeployInput): Promise<IContracts> {
   await _omniRegistrarSynchronizer.deployed();
   const omniRegistrarSynchronizer = OmniRegistrarSynchronizerFactory.attach(_omniRegistrarSynchronizer.address);
   console.log(`[[${NetworkConfig[input.network].name}]] OmniRegistrarSynchronizer Deployed - ${omniRegistrarSynchronizer.address}`);
+  return omniRegistrarSynchronizer;
+}
 
-  // Omni Registrar
+export interface IDeployOmniRegistrarInput extends IDeployInput {
+  Registry: Registry;
+  OmniRegistrarSynchronizer: OmniRegistrarSynchronizer;
+}
+
+// Omni Registrar
+export async function deployOmniRegistrar(input: IDeployOmniRegistrarInput): Promise<OmniRegistrar> {
   const OmniRegistrarFactory = await ethers.getContractFactory("OmniRegistrar", input.signer);
-  const _omniRegistrar = await upgrades.deployProxy(OmniRegistrarFactory, [registry.address, omniRegistrarSynchronizer.address]);
+  const _omniRegistrar = await upgrades.deployProxy(OmniRegistrarFactory, [input.Registry.address, input.OmniRegistrarSynchronizer.address]);
   await _omniRegistrar.deployed();
   const omniRegistrar = OmniRegistrarFactory.attach(_omniRegistrar.address);
   console.log(`[[${NetworkConfig[input.network].name}]] OmniRegistrar Deployed - ${omniRegistrar.address}`);
+  return omniRegistrar;
+}
 
-  // Omni Registrar Controller
+export interface IDeployOmniRegistrarControllerInput extends IDeployInput {
+  Token: Token;
+  DomainPriceOracle: DomainPriceOracle;
+  TokenPriceOracle: TokenPriceOracle;
+  OmniRegistrar: OmniRegistrar;
+}
+
+// Omni Registrar Controller
+export async function deployOmniRegistrarControllers(input: IDeployOmniRegistrarControllerInput): Promise<OmniRegistrarController> {
   const OmniRegistrarControllerFactory = await ethers.getContractFactory("OmniRegistrarController", input.signer);
   const _omniRegistrarController = await upgrades.deployProxy(OmniRegistrarControllerFactory, [
-    token.address,
-    domainPriceOracle.address,
-    tokenPriceOracle.address,
-    omniRegistrar.address,
+    input.Token.address,
+    input.DomainPriceOracle.address,
+    input.TokenPriceOracle.address,
+    input.OmniRegistrar.address,
     input.networks.map((network_) => NetworkConfig[network_].slip44.coinId),
   ]);
   await _omniRegistrarController.deployed();
   const omniRegistrarController = OmniRegistrarControllerFactory.attach(_omniRegistrarController.address);
   console.log(`[[${NetworkConfig[input.network].name}]] OmniRegistrarController Deployed - ${omniRegistrarController.address}`);
+  return omniRegistrarController;
+}
 
-  // Root
+export interface IDeployRootInput extends IDeployInput {
+  Registry: Registry;
+  SingletonRegistrar: SingletonRegistrar;
+  OmniRegistrar: OmniRegistrar;
+}
+
+// Root
+export async function deployRoot(input: IDeployRootInput): Promise<Root> {
   const RootFactory = await ethers.getContractFactory("Root", input.signer);
   const _root = await upgrades.deployProxy(RootFactory, [
-    registry.address,
-    singletonRegistrar.address,
-    omniRegistrar.address,
+    input.Registry.address,
+    input.SingletonRegistrar.address,
+    input.OmniRegistrar.address,
     NetworkConfig[input.network].layerzero.endpoint.address,
     NetworkConfig[input.network].layerzero.chainId,
     input.networks.map((network_) => NetworkConfig[network_].layerzero.chainId),
@@ -122,19 +206,5 @@ export async function deploy(input: IDeployInput): Promise<IContracts> {
   await _root.deployed();
   const root = RootFactory.attach(_root.address);
   console.log(`[[${NetworkConfig[input.network].name}]] Root Deployed - ${root.address}`);
-
-  return {
-    Token: token,
-    Registry: registry,
-    PublicResolver: publicResolver,
-    PublicResolverSynchronizer: publicResolverSynchronizer,
-    SingletonRegistrar: singletonRegistrar,
-    SingletonRegistrarController: singletonRegistrarController,
-    OmniRegistrar: omniRegistrar,
-    OmniRegistrarController: omniRegistrarController,
-    OmniRegistrarSynchronizer: omniRegistrarSynchronizer,
-    TokenPriceOracle: tokenPriceOracle,
-    DomainPriceOracle: domainPriceOracle,
-    Root: root,
-  };
+  return root;
 }
--- scripts/helpers/load.ts
+++ scripts/helpers/load.ts
@@ -4,17 +4,14 @@ import Contracts from "../../static/contracts.json";
 import { createProvider } from "../helpers/provider";
 import { ethers } from "hardhat";
 import { IDeployedContracts } from "../interfaces/deployed-contracts";
+import { IContracts } from "../interfaces/contracts";
 
-export interface ILoadInput {
-  networks: Network[];
-  signer: SignerWithAddress;
-}
-export const load = async (input: ILoadInput): Promise<IDeployedContracts> => {
+export const fullLoad = async (networks: Network[], signer: SignerWithAddress): Promise<IDeployedContracts> => {
   const contracts: IDeployedContracts = {};
 
-  for (const network of input.networks) {
+  for (const network of networks) {
     const provider_ = createProvider(network);
-    const signer_ = input.signer.connect(provider_);
+    const signer_ = signer.connect(provider_);
     const contracts_ = Contracts[network];
 
     const TokenFactory = await ethers.getContractFactory("Token", signer_);
@@ -23,23 +20,106 @@ export const load = async (input: ILoadInput): Promise<IDeployedContracts> => {
     const TokenPriceOracleFactory = await ethers.getContractFactory("TokenPriceOracle", signer_);
     const TokenPriceOracle = TokenPriceOracleFactory.attach(contracts_.TokenPriceOracle);
 
-    // TODO:
+    const DomainPriceOracleFactory = await ethers.getContractFactory("DomainPriceOracle", signer_);
+    const DomainPriceOracle = DomainPriceOracleFactory.attach(contracts_.DomainPriceOracle);
 
-    // contracts[network] = {
-    //   Token,
-    //   Registry,
-    //   PublicResolver,
-    //   PublicResolverSynchronizer,
-    //   SingletonRegistrar,
-    //   SingletonRegistrarController,
-    //   OmniRegistrar,
-    //   OmniRegistrarController,
-    //   OmniRegistrarSynchronizer,
-    //   TokenPriceOracle,
-    //   DomainPriceOracle,
-    //   Root,
-    // };
+    const RegistryFactory = await ethers.getContractFactory("Registry", signer_);
+    const Registry = RegistryFactory.attach(contracts_.Registry);
+
+    const PublicResolverSynchronizerFactory = await ethers.getContractFactory("PublicResolverSynchronizer", signer_);
+    const PublicResolverSynchronizer = PublicResolverSynchronizerFactory.attach(contracts_.PublicResolverSynchronizer);
+
+    const PublicResolverFactory = await ethers.getContractFactory("PublicResolver", signer_);
+    const PublicResolver = PublicResolverFactory.attach(contracts_.PublicResolver);
+
+    const SingletonRegistrarFactory = await ethers.getContractFactory("SingletonRegistrar", signer_);
+    const SingletonRegistrar = SingletonRegistrarFactory.attach(contracts_.SingletonRegistrar);
+
+    const SingletonRegistrarControllerFactory = await ethers.getContractFactory("SingletonRegistrarController", signer_);
+    const SingletonRegistrarController = SingletonRegistrarControllerFactory.attach(contracts_.SingletonRegistrarController);
+
+    const OmniRegistrarSynchronizerFactory = await ethers.getContractFactory("OmniRegistrarSynchronizer", signer_);
+    const OmniRegistrarSynchronizer = OmniRegistrarSynchronizerFactory.attach(contracts_.OmniRegistrarSynchronizer);
+
+    const OmniRegistrarFactory = await ethers.getContractFactory("OmniRegistrar", signer_);
+    const OmniRegistrar = OmniRegistrarFactory.attach(contracts_.OmniRegistrar);
+
+    const OmniRegistrarControllerFactory = await ethers.getContractFactory("OmniRegistrarController", signer_);
+    const OmniRegistrarController = OmniRegistrarControllerFactory.attach(contracts_.OmniRegistrarController);
+
+    const RootFactory = await ethers.getContractFactory("Root", signer_);
+    const Root = RootFactory.attach(contracts_.Root);
+
+    contracts[network] = {
+      Token,
+      Registry,
+      PublicResolver,
+      PublicResolverSynchronizer,
+      SingletonRegistrar,
+      SingletonRegistrarController,
+      OmniRegistrar,
+      OmniRegistrarController,
+      OmniRegistrarSynchronizer,
+      TokenPriceOracle,
+      DomainPriceOracle,
+      Root,
+    };
   }
 
   return contracts;
 };
+
+export async function load(network: Network, signer: SignerWithAddress): Promise<IContracts> {
+  const contracts_ = Contracts[network];
+
+  const TokenFactory = await ethers.getContractFactory("Token", signer);
+  const Token = TokenFactory.attach(contracts_.Token);
+
+  const TokenPriceOracleFactory = await ethers.getContractFactory("TokenPriceOracle", signer);
+  const TokenPriceOracle = TokenPriceOracleFactory.attach(contracts_.TokenPriceOracle);
+
+  const DomainPriceOracleFactory = await ethers.getContractFactory("DomainPriceOracle", signer);
+  const DomainPriceOracle = DomainPriceOracleFactory.attach(contracts_.DomainPriceOracle);
+
+  const RegistryFactory = await ethers.getContractFactory("Registry", signer);
+  const Registry = RegistryFactory.attach(contracts_.Registry);
+
+  const PublicResolverSynchronizerFactory = await ethers.getContractFactory("PublicResolverSynchronizer", signer);
+  const PublicResolverSynchronizer = PublicResolverSynchronizerFactory.attach(contracts_.PublicResolverSynchronizer);
+
+  const PublicResolverFactory = await ethers.getContractFactory("PublicResolver", signer);
+  const PublicResolver = PublicResolverFactory.attach(contracts_.PublicResolver);
+
+  const SingletonRegistrarFactory = await ethers.getContractFactory("SingletonRegistrar", signer);
+  const SingletonRegistrar = SingletonRegistrarFactory.attach(contracts_.SingletonRegistrar);
+
+  const SingletonRegistrarControllerFactory = await ethers.getContractFactory("SingletonRegistrarController", signer);
+  const SingletonRegistrarController = SingletonRegistrarControllerFactory.attach(contracts_.SingletonRegistrarController);
+
+  const OmniRegistrarSynchronizerFactory = await ethers.getContractFactory("OmniRegistrarSynchronizer", signer);
+  const OmniRegistrarSynchronizer = OmniRegistrarSynchronizerFactory.attach(contracts_.OmniRegistrarSynchronizer);
+
+  const OmniRegistrarFactory = await ethers.getContractFactory("OmniRegistrar", signer);
+  const OmniRegistrar = OmniRegistrarFactory.attach(contracts_.OmniRegistrar);
+
+  const OmniRegistrarControllerFactory = await ethers.getContractFactory("OmniRegistrarController", signer);
+  const OmniRegistrarController = OmniRegistrarControllerFactory.attach(contracts_.OmniRegistrarController);
+
+  const RootFactory = await ethers.getContractFactory("Root", signer);
+  const Root = RootFactory.attach(contracts_.Root);
+
+  return {
+    Token,
+    Registry,
+    PublicResolver,
+    PublicResolverSynchronizer,
+    SingletonRegistrar,
+    SingletonRegistrarController,
+    OmniRegistrar,
+    OmniRegistrarController,
+    OmniRegistrarSynchronizer,
+    TokenPriceOracle,
+    DomainPriceOracle,
+    Root,
+  };
+}
