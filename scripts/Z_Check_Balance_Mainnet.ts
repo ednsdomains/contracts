81c72f13aa0682a19b1cc0ff31cfd2cb8c2f6925
Mike Ng <iam@mikeng.io>
2023-04-21 19:27:39 +0800

    Added script for checking the mainnet address balance

---
+++ scripts/Z_Check_Balance_Mainnet.ts
@@ -0,0 +1,25 @@
+import { ethers } from "hardhat";
+import NetworkConfig, { Network, Mainnets } from "../network.config";
+
+async function main() {
+  const _signer = new ethers.Wallet(process.env.PRIVATE_KEY!);
+  const getContent = async (network: Network) => {
+    const provider = new ethers.providers.JsonRpcProvider(NetworkConfig[network].url);
+    const balance = await provider.getBalance(_signer.address);
+    return { name: NetworkConfig[network].name, symbol: NetworkConfig[network].symbol, balance: ethers.utils.formatEther(balance) };
+  };
+  const data = [];
+  for (const network in NetworkConfig) {
+    if (Mainnets.find((t) => t === NetworkConfig[network].chainId) && NetworkConfig[network] && NetworkConfig[network].url) {
+      data.push(getContent(NetworkConfig[network].chainId));
+    }
+  }
+  const result = await Promise.all(data);
+  console.log(`Account: ${_signer.address}`);
+  console.table(result);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
