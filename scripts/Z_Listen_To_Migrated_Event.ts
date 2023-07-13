f23e91e3a5ac1ef3ea2be872da0bcec9a2a77d2a
Mike Ng <iam@mikeng.io>
2023-07-13 15:56:27 +0800

    Added a test listener event to `MigrationManager`

--- scripts/Z_Listen_To_Migrated_Event.ts
+++ scripts/Z_Listen_To_Migrated_Event.ts
@@ -2,22 +2,17 @@ import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { getMigrationManager } from "./src/lib/get-contracts";
 
-const BASE_REGISTRAR_IMPLEMENTATION = "0xafFDDAd389bEe8a2AcBa0367dFAE5609B93c7F9b";
-
 async function main() {
   const [signer] = await ethers.getSigners();
 
   const migrator = await getMigrationManager(signer);
   if (!migrator) throw new Error("Migrator not found");
 
-  const tx = await migrator.migrate(
-    BASE_REGISTRAR_IMPLEMENTATION,
-    BigNumber.from("109783084056604531771219068466088667082536763616297497846805007741235889629597"),
-    "testmigration1",
-    "meta",
-  );
-  console.log(`Hash: ${tx.hash}`);
-  await tx.wait();
+  const filter = migrator.filters["Migrated"]();
+  console.log(`Start listening for Migrated events...`);
+  migrator.on(filter, (owner, name, tld, oldTokenId) => {
+    console.log(owner, name, tld, oldTokenId);
+  });
 }
 
 main().catch((error) => {
